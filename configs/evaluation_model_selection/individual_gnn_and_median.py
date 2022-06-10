dd44a7618bb2690c07798bd1c889b9b2cd82870a
Philipp Hummler <philipp.hummler@me.com>
2022-06-11 00:07:28 +0200

    specified comparison to median in individual gnn evaluation configs

--- configs/evaluation_model_selection/individual_gnn_and_median.py
+++ configs/evaluation_model_selection/individual_gnn_and_median.py
@@ -1,24 +1,24 @@
-from configs.model.gnn_variables_1 import model_config as gnn_variables_1_model_config
+from configs.model.gnn import model_config as gnn_model_config
 from utils.configs.evaluation_model import EvaluationModelConfig
 from utils.configs.evaluation_model_selection import EvaluationModelSelectionConfig
 
 
 evaluation_model_selection_config = EvaluationModelSelectionConfig(
-    name=f"individual_{gnn_variables_1_model_config.name}",
+    name=f"individual_{gnn_model_config.name}_and_median",
     evaluation_model_configs=[
         EvaluationModelConfig(
-            model_config=gnn_variables_1_model_config,
+            model_config=gnn_model_config,
             run_selection="all",
             run_aggregation="median",
             is_comparison_base=True,
-            display_name="GNN2",
+            display_name="GNN1",
         ),
         EvaluationModelConfig(
-            model_config=gnn_variables_1_model_config,
+            model_config=gnn_model_config,
             run_selection="all",
             run_aggregation="individual",
             is_comparison_base=False,
-            display_name="GNN 2",
+            display_name="GNN 1",
         ),
     ],
 )
